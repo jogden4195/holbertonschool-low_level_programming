30261428e649ddce8367ee44060102394f8b1321
Jenn <jennogden95@gmail.com>
2018-06-21 19:47:59 +0000

    Hopefully fixed task 2 (I forgot to include a variable in one of the print statements).

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
+		int last_digit = n % 10;
+
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
 	else if ((n % 10) == 0)
 	{
@@ -23,7 +25,7 @@ int main(void)
 	}
 	else if ((n % 10) < 6)
 	{
-		printf("Last digit of %d is less than 6 and not 0\n", n, (n % 10))
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10))
 	}
 	return (0);
 }
