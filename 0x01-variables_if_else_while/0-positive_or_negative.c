643b2b34be50dfed43ce843bb50d8593347a990b
Jenn <jennogden95@gmail.com>
2018-06-21 18:27:30 +0000

    Adding README and task 1.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Generates a random int and prints if it's +,-, or 0
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
+	else
+	{
+		printf("%d is positive", n);
+	}
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+# 0x01. C - Variables, if, else, while
+
+This project will help us understand more of the basics of the C programming language such as variables and if, else, & while loops. 
