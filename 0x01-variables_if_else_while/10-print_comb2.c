16c5c514c119f31f0f97d60d0f037131a23e0e7c
Jenn <jennogden95@gmail.com>
2018-06-22 00:34:43 +0000

    Adding task 10.

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the numbers 00 to 99
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i, n;
+	char first_int, second_int;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (n = 0; n <= 9; n++)
+		{
+			putchar(first_int);
+			putchar(second_int);
+			if (i != 9 || n != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			second_int++;
+		}
+		first_int++;
+		second_int = 0;
+	}
+	putchar('\n');
+	return (0);
+}
