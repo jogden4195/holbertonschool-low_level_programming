8b520ecdb430667392aa0282c8d392d001491451
Jenn <jennogden95@gmail.com>
2018-06-22 00:54:23 +0000

    hopefully fixed task 10.

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -8,24 +8,20 @@
 int main(void)
 {
 	int i, n;
-	char first_int = '0';
-	char second_int = '0';
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (n = 0; n <= 9; n++)
+		for (n = '0'; n <= '9'; n++)
 		{
-			putchar(first_int);
-			putchar(second_int);
-			if (i != 9 || n != 9)
+			putchar(i);
+			putchar(n);
+			if (i != '9' || n != '9')
 			{
 				putchar(',');
 				putchar(' ');
 			}
-			second_int++;
 		}
-		first_int++;
-		second_int = '0';
+		n = '0';
 	}
 	putchar('\n');
 	return (0);
