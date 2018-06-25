c5d8d675a80f438dd89d25b9b4d36b8e9968ab6c
Jenn <jennogden95@gmail.com>
2018-06-25 17:40:25 +0000

    Adding task 0 and README.

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints Holberton followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Holberton");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+# 0x02. C - Functions, nested loops
+
+This project will help us understand functions and nested loops in C.
