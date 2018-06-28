4480461a7053f7fbefd95fe116db8029d743ee69
Jenn <jennogden95@gmail.com>
2018-06-28 18:20:10 +0000

    Adding task 3.

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers 0-9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
