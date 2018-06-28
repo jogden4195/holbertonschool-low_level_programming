2690d943535cfe8bd37e2c4cfb24179cf7fdb11a
Jenn <jennogden95@gmail.com>
2018-06-28 23:21:04 +0000

    Fixed the extra 0 for task 12

--- 0x03-more_functions_nested_loops/main.c
+++ 0x03-more_functions_nested_loops/main.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
-
+#include <limits.h>
 /**
  * main - check the code for Holberton School students.
  *
@@ -17,6 +17,10 @@ int main(void)
   print_number(0);
   _putchar('\n');
   print_number(-98);
+  _putchar('\n');
+  print_number(INT_MAX);
+  _putchar('\n');
+  print_number(INT_MIN);
   _putchar('\n');
   return (0);
 }
