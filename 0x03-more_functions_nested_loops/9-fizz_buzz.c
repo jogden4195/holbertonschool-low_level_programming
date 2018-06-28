6cd153773d0f3770909e4fbd0f6ce64590dff050
Jenn <jennogden95@gmail.com>
2018-06-28 20:05:39 +0000

    Adding task 9.

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * Fizz-Buzz: Performs the Fizz-Buzz test
+ *
+ * Return: none
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i=1; i <= 100; i++)
+	{
+		if ((i % 3) == 0)
+		{
+			if ((i % 5) == 0)
+				printf("FizzBuzz ");
+			else
+				printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			if (i == 100)
+				printf("Buzz\n");
+			else
+				printf("Buzz ");
+		}
+		else
+			printf("%d ", i);
+	}
+	return (0);
+}
