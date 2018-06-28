26d47338ab9685483a75fcde72fff2cabcc3efa7
Jenn <jennogden95@gmail.com>
2018-06-28 21:17:51 +0000

    Fixed task 7

--- 0x03-more_functions_nested_loops/7-print_diagonal.c
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -2,7 +2,7 @@
 
 /**
  * print_diagonal - prints a diagonal line n \ long
- * @n - how long the line will be
+ * @n: how long the line will be
  *
  * Return: none
  */
@@ -14,7 +14,7 @@ void print_diagonal(int n)
 		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
-		for (a = 0; a <= i; a++)
+		for (a = 0; a < i; a++)
 			_putchar(' ');
 		_putchar('\\');
 		_putchar('\n');
