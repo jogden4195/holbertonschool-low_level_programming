14d3dc4912d8a4ce8912ceee317c767a3180fac8
Jenn <jennogden95@gmail.com>
2018-07-01 21:34:18 +0000

    Adding task 13.

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int multiplier, product, i;
+
+	for (multiplier = 0; multiplier <= 9; multiplier++)
+	{
+		for (i = 0; i <= 9; i++)
+		{
+			product = multiplier * i;
+			if (product <= 9)
+			{
+				if (i != 0)
+					_putchar(' ');
+				_putchar('0' + product);
+				if (i != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
+				if (product != 81)
+				{
+					if (i != 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
+				}
+			}
+		}
+		_putchar('\n');
+		product = 0;
+	}
+}
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-  times_table();
-  return (0);
+  int r;
+
+    r = _islower('H');
+    _putchar(r + '0');
+    r = _islower('o');
+    _putchar(r + '0');
+    r = _islower(108);
+    _putchar(r + '0');
+    _putchar('\n');
+    return (0);
 }
