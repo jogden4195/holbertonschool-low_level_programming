531118851b1a3cf2efdaf7b7634508225a26b84e
Jenn <jennogden95@gmail.com>
2018-07-01 21:36:01 +0000

    Adding task 13

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b, i;
+
+	for (a = 0; a <= 98; a++)
+	{
+		i = a + 1;
+		for (b = i; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
+}
