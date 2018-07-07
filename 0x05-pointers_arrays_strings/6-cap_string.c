54b57509c2fba6d8bac009c392d4aad3b65b3849
Jenn <jennogden95@gmail.com>
2018-07-07 21:13:59 +0000

    Added vertical tabs to trigger list for 6.

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -10,11 +10,11 @@
 char *cap_string(char *str)
 {
 	int i = 0, j = 0;
-	int array[] = {9, 10, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+	int array[] = {9, 10, 11, 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
 
 	while (str[i] != '\0')
 	{
-		for (j = 0; j < 13; j++)
+		for (j = 0; j < 14; j++)
 		{
 			if (str[i] == array[j])
 			{
