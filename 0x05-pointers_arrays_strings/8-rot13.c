74a7e5b53ab8253987ac62981453c20faa889166
Jenn <jennogden95@gmail.com>
2018-07-08 00:21:14 +0000

    Adding task 8

---
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+/**
+ * *rot13 - encodes a string using rot13
+ * @str: string that we are encoding
+ *
+ * Return: char
+ */
+
+char *rot13(char *str)
+{
+	int i = 0, j;
+	char letters1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char letters2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	
+	while (str[i] != '\0')
+	{
+		for(j = 0; j < 52; j++)
+		{
+		  	if (str[i] == letters1[j])
+			{
+				str[i] = letters2[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (str);
+}
