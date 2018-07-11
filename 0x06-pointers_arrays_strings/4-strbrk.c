cd50e0680da2a27a6b568b2f374cf68a183e1a02
Jenn <jennogden95@gmail.com>
2018-07-11 05:53:18 +0000

    Hopefully fixed 4.

--- 0x06-pointers_arrays_strings/4-strbrk.c
+++ 0x06-pointers_arrays_strings/4-strbrk.c
@@ -11,14 +11,19 @@ char *_strpbrk(char *s, char *accept)
 	char *target = 0;
 	int count = 0, i = 0, j;
 
-        while (accept[count] != '\0')
+	while (accept[count] != '\0')
 		count++;
-        while (s[i] != '\0')
+	if (count == 0)
+		return (target);
+	while (s[i] != '\0')
 	{
 		for (j = 0; j <= count; j++)
 		{
 			if (s[i] == accept[j])
+			{
 				target = &s[i];
+				return (target);
+			}
 		}
 		i++;
 	}
