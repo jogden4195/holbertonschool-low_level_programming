861dc1e0556dd8c9b22f7003d327662931bf99df
Jenn <jennogden95@gmail.com>
2018-07-12 06:25:39 +0000

    Fixed betty errors for 5.

--- 0x07-recursion/5-sqrt_recursion.c
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -2,15 +2,23 @@
 int roots(int n, int y);
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number we are finding the sqrt for 
+ * @n: number we are finding the sqrt for
  *
  * Return: the natural sqrt of n or -1 if n does not have one (int)
  */
 
 int _sqrt_recursion(int n)
 {
-	return(roots(n, 1)); 
+	return (roots(n, 1));
 }
+
+/**
+ * roots - calculates whether a number is a sqrt
+ * @x: number we are trying to find sqrt for
+ * @y: number we are testing to see if is a root of x
+ *
+ * Return: value of y if root, -1 if no root (int)
+ */
 
 int roots(int x, int y)
 {
