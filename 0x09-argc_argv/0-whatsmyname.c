313cb49e77fba835cd516159c5da1202762c4bee
Jenn <jennogden95@gmail.com>
2018-07-13 19:59:55 +0000

    Adding task 1

--- 0x09-argc_argv/0-whatsmyname.c
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -2,15 +2,17 @@
 
 /**
  * main - prints name of program, followed by a new line.
+ * @argc: number of arguments given to main
+ * @argv: array of arguments given to main in the form of strings
  *
  * Return: 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	if(argc > 0)
+	if (argc > 0)
 	{
-  		printf("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 		argc++;
 	}
 	return (0);
