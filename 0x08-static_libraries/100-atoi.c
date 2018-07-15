ff26246eeca5be3f1c25002ebbed6dab19ccecd2
Jenn <jennogden95@gmail.com>
2018-07-15 05:47:11 +0000

    Possibly fixed problem for 0

--- 0x08-static_libraries/100-atoi.c
+++ 0x08-static_libraries/100-atoi.c
@@ -1,1 +1,18 @@
-int _atoi(char *s);
+#include "holberton.h"
+
+int _atoi(char *s)
+{
+	int num = 0, sign = 1, i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (s[i] != '\0')
+	{
+		num = num * 10 + s[i] - '0';
+		i++;
+	}
+	return (num * sign);
+}
