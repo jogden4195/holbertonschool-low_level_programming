ce40c5555fcf98216cc51d26b3e2a74be3d6152e
Jenn <jennogden95@gmail.com>
2018-07-17 18:51:54 +0000

    Adding task 1.

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+int _strlen(char *s);
+/**
+ * *strdup - returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a
+ * parameter
+ * @str: string we are copying into new array
+ *
+ * Return: pointer to new array if success (char), NULL if str
+ * is empty or insufficient memory was available
+ */
+char *_strdup(char *str)
+{
+	char *t;
+	int i = 0, size;
+
+	if (*str == '\0')
+		return (NULL);
+	size = _strlen(str) + 1;
+	t = malloc(size * sizeof(char));
+	if (t == NULL)
+		return ('\0');
+	while (str[i] != '\0')
+	{
+		t[i] = str[i];
+		i++;
+	}
+	t[i] = '\0';
+  	return (t);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string we are checking the length of
+ *
+ * Return: Length (int)
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	return (length);
+}
