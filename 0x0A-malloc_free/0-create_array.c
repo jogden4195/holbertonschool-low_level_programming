9dad4109693935f24c50a6fc996c1fc311752407
Jenn <jennogden95@gmail.com>
2018-07-17 18:19:17 +0000

    Changed <= to < in for loop

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * *create_array - creates an array of chars and initializes it
@@ -17,13 +18,8 @@ char *create_array(unsigned int size, char c)
 
 	t = malloc(sizeof(char) * size);
 	if (size == 0)
-	{
 		return ('\0');
-	}
-	while (i <= size)
-	{
+	for (i = 0; i < size; i++)
 		t[i] = c;
-		i++;
-	}
 	return (t);
 }
