ece29a986486d77e8d3c4728e3a670d02f671942
Jenn <jennogden95@gmail.com>
2018-07-17 18:28:11 +0000

    Forgot to check if malloc failed.

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -16,8 +16,10 @@ char *create_array(unsigned int size, char c)
 	char *t;
 	unsigned int i = 0;
 
-	t = malloc(sizeof(char) * size);
 	if (size == 0)
+		return ('\0');
+	t = malloc(sizeof(*t) * size);
+	if (t == NULL)
 		return ('\0');
 	for (i = 0; i < size; i++)
 		t[i] = c;
