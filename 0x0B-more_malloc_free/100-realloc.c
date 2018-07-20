b22f6a8c302725fa997a18111a426c9ba5cc95ed
Jenn <jennogden95@gmail.com>
2018-07-20 14:38:19 +0000

    Added comments, fixed initialization loop (so no memory leaks), and fixed betty errors.

--- 0x0B-more_malloc_free/100-realloc.c
+++ 0x0B-more_malloc_free/100-realloc.c
@@ -12,33 +12,54 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *t;
-	char *p;
-	char *pptr;
-	unsigned int size;
+	void *t;    /*This is going to be our new array */
+	char *p;    /*This pointer will be used to initialize our new array*/
+	char *pptr; /*We're gonna use this to go through our old array */
+	unsigned int size; /*this will be used for our initialization loop*/
 
-	if (new_size == old_size)
+	if (new_size == old_size)    /*case where new_size = old_size*/
 		return (ptr);
-	if (new_size == 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL) /*
+					   *case where we have ptr but new_size = 0.
+					   *we basically just want to free the ptr.
+					   */
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (ptr == NULL)
+	if (ptr == NULL)   /*
+			    *when ptr is NULL, we want to malloc(newsize) and return
+			    *that new memory space
+			    */
 	{
 		t = malloc(new_size);
 		if (t == NULL)
 			return (NULL);
 		return (t);
 	}
-	t = malloc(new_size);
+	t = malloc(new_size);  /*
+				*if our args didn't fall under any of the above cate-
+				*gories, we're gonna malloc the new size and check
+				*if it actually passed malloc
+				*/
 	if (t == NULL)
 		return (NULL);
-	size = new_size;
-	p = t;
-	pptr = ptr;
-	for (p = t; size--;)
+	size = new_size;       /*
+				*we will end our upcoming loop when size = 0. Since
+				*we cannot actually change the value of new_size,
+				*we gotta set another changable variable to the same
+				*value of new_size
+				*/
+	p = t;     /*this pointer will initialize our new memory space*/
+	pptr = ptr;  /*
+		      *this pointer will "dereference" our ptr void pointer so we can
+		      *initialize t with it's elements
+		      */
+	while (size--)
 		*p++ = *pptr++;
-	free(ptr);
+	free(ptr);  /*
+		     *since we have no more use for ptr, we can free the memory for
+		     *it
+		     */
 	return (t);
 }
