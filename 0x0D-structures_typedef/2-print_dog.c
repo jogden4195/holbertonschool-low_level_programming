2d623dec2c86a443321e6de3adf3aa49180ecad2
Jenn <jennogden95@gmail.com>
2018-07-23 19:58:17 +0000

    Forgot conditionals on 2

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,8 +10,21 @@
 
 void print_dog(struct dog *d)
 {
-	printf("Name: %s\n", d->name);
+	if (d == NULL)
+		return;
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	/*
+	 *	if (d->age == NULL)
+	 *	printf("Age: (nil)\n");
+	 *else
+	 */
 	printf("Age: %.6f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
 
