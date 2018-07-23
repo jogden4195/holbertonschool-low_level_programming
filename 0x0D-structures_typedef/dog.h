5fbb29f1bb83268ecac13d157a88d3450aac3a71
Jenn <jennogden95@gmail.com>
2018-07-23 19:11:12 +0000

    Adding README and dog.h.

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - info about a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner of dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
