8b3073fdc78e6d744e6ea3507ec6ee460e01f05e
Jenn <jennogden95@gmail.com>
2018-07-23 22:35:58 +0000

    Created copies for owner and name.

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+int _strlen(char *s);
 
 /**
  * new_dog - creates a new dog
@@ -10,15 +11,60 @@
  * Return: new dog_t or NULL if function fails
  */
 
-struct dog *new_dog(char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+	int i = 0, name_len, owner_len;
+	char *name_cp;
+	char *owner_cp;
+	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	if (name != NULL)
+	{
+		name_len = _strlen(name);
+		name_cp = malloc(sizeof(char) * name_len);
+		if (name_cp == NULL)
+			return (NULL);
+		while (name[i] != '\0')
+		{
+			name_cp[i] = name[i];
+			i++;
+		}
+		name_cp[i] = '\0';
+		new_dog->name = name_cp;
+	}
 	new_dog->age = age;
-	new_dog->owner = owner;
+	if (owner != NULL)
+	{
+		owner_len = _strlen(owner);
+		owner_cp = malloc(sizeof(char) * owner_len);
+		if (owner_cp == NULL)
+			return (NULL);
+		while (owner[i] != '\0')
+		{
+			owner_cp[i] = owner[i];
+			i++;
+		}
+		owner_cp[i] = '\0';
+		new_dog->owner = owner_cp;
+	}
 	return (new_dog);
+}
+
+/**
+ * _strlen - gets the length of the str
+ * @s: string we are getting length of
+ *
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+	return (length);
 }
