be3ec243557c63d8f324fabb0391824bd9dae7fb
Jenn <jennogden95@gmail.com>
2018-07-23 23:44:28 +0000

    I think I actually fixed 4.

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -18,61 +18,46 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *owner_cp;
 	dog_t *new_dog;
 
+	if (name == NULL || owner == NULL)
+		return (NULL);
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	if (name != NULL)
-	{
-		name_len = _strlen(name);
-		if (name_len == 0)
-			name_len = 1;
-		name_cp = malloc(sizeof(char) * name_len);
-		if (name_cp == NULL)
-		{
-			free(new_dog);
-			return (NULL);
-		}
-		while (name[i] != '\0')
-		{
-			name_cp[i] = name[i];
-			i++;
-		}
-		name_cp[i] = '\0';
-		new_dog->name = name_cp;
-	}
-	else
+	name_len = _strlen(name);
+	if (name_len == 0)
+		name_len = 1;
+	name_cp = malloc(sizeof(char) * name_len);
+	if (name_cp == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	new_dog->age = age;
-	if (owner != NULL)
+	while (name[i] != '\0')
 	{
-		owner_len = _strlen(owner);
-		if (owner_len == 0)
-			owner_len = 1;
-		owner_cp = malloc(sizeof(char) * owner_len);
-		if (owner_cp == NULL)
-		{
-			free(name_cp);
-			free(new_dog);
-			return (NULL);
-		}
-		i = 0;
-		while (owner[i] != '\0')
-		{
-			owner_cp[i] = owner[i];
-			i++;
-		}
-		owner_cp[i] = '\0';
-		new_dog->owner = owner_cp;
+		name_cp[i] = name[i];
+		i++;
 	}
-	else
+	name_cp[i] = '\0';
+	new_dog->name = name_cp;
+	new_dog->age = age;
+	owner_len = _strlen(owner);
+	if (owner_len == 0)
+		owner_len = 1;
+	owner_cp = malloc(sizeof(char) * owner_len);
+	if (owner_cp == NULL)
 	{
 		free(name_cp);
 		free(new_dog);
 		return (NULL);
 	}
+	i = 0;
+	while (owner[i] != '\0')
+	{
+		owner_cp[i] = owner[i];
+		i++;
+	}
+	owner_cp[i] = '\0';
+	new_dog->owner = owner_cp;
 	return (new_dog);
 }
 
