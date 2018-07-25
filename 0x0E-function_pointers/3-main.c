9f32bab075c18b1b5d9c8b1bfe455bfbe2823f19
Jenn <jennogden95@gmail.com>
2018-07-25 22:09:31 +0000

    Adding all files of task 3.

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - performs simple math operations and prints result
+ * @argc: number of arguments given to main
+ * @argv: array of arguments
+ *
+ * Return: none
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	printf("%d\n", get_op_func(argv[2])(a, b));
+	return (0);
+}
