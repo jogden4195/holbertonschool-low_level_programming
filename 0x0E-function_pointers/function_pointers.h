de76cef69c78ab70070a26682679e25bd465be11
Jenn <jennogden95@gmail.com>
2018-07-25 18:11:56 +0000

    Adding task 1 and updated header files.

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+#include <stdlib.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-/*void array_iterator(int *array, size_t size, void (*action)(int));*/
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 int op_add(int a, int b);
 int op_sub(int a, int b);
