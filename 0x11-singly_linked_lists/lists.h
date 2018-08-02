b4f515933c1e93db0820a567ecd0f3c31705e9ff
Jenn <jennogden95@gmail.com>
2018-08-02 20:18:12 +0000

    Fixed betty errors.

--- 0x11-singly_linked_lists/lists.h
+++ 0x11-singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -13,9 +14,9 @@
  */
 typedef struct list_s
 {
-  char *str;
-  unsigned int len;
-  struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
