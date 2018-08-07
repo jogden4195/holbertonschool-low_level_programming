8f26609da99847d2cef0f670c6a641cc15f7bbeb
Jenn <jennogden95@gmail.com>
2018-08-07 08:20:56 +0000

    Deleted head = NULL line at end of function.

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,14 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *next_node;
+	listint_t *next_node = NULL;
 
+	if (head == NULL)
+		return;
 	while (*head != NULL)
 	{
 		next_node = (*head)->next;
 		free(*head);
 		*head = next_node;
 	}
-	head = NULL;
 }
