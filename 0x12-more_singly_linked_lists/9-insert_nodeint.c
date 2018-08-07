ee62834ef025e7b502ff880380e600bf941be457
Jenn <jennogden95@gmail.com>
2018-08-07 16:34:04 +0000

    Added case where head == NULL

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,21 +15,35 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node;
 	listint_t *next_node;
 	listint_t *after_new;
-	unsigned int i;
+	unsigned int i = 0;
 
-        new_node = malloc(sizeof(listint_t));
-        if (new_node == NULL)
+	if (head == NULL)
 		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+	if (idx == 0 && *head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
 	next_node = *head;
-	for (i = 0; i < idx - 1; i++)
+	while (i < idx - 1)
 	{
 		next_node = next_node->next;
 		if (next_node == NULL)
+		{
+			free(new_node);
 			return (NULL);
+		}
+		i++;
 	}
 	after_new = next_node->next;
-	next_node->next = new_node;
-	new_node->n = n;
+	if (idx == 0)
+		*head = new_node;
+	else
+		next_node->next = new_node;
 	new_node->next = after_new;
 	return (new_node);
-} 
+}
