c8ccb7f8919a0ef9eacf694be2a0deabec819b80
Jenn <jennogden95@gmail.com>
2018-08-07 04:34:21 +0000

    Adding task 4.

---
+++ 0x12-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t
+ * @head: first node in listint_t list
+ *
+ * Return: none
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *next_node;
+
+	while (head != NULL)
+	{
+		next_node = head->next;
+		free(head);
+		head = next_node;
+	}
+}
