d66d100a012bb8a212aa5ae45faf82a048aafd34
Jenn <jennogden95@gmail.com>
2018-08-09 09:44:32 +0000

    Adding task 1

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number we are converting into binary and printing
+ *
+ * Return: none
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned int i = 32768, flag = 0; /* binary value of i: 1000 0000 0000 0000 */
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (i > 0)
+	{
+		if ((n & i) == 0)
+		{
+			if (flag == 1)
+				_putchar('0');
+		}
+		else
+		{
+			_putchar('1');
+			flag = 1;
+		}
+		i >>= 1;
+	}
+}
