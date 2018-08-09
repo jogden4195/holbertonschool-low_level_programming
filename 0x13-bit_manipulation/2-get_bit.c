aafbd3c824a596382f53bacbdb5ab56dd7a2e2cb
Jenn <jennogden95@gmail.com>
2018-08-09 20:40:22 +0000

    Added check for valid index.

--- 0x13-bit_manipulation/2-get_bit.c
+++ 0x13-bit_manipulation/2-get_bit.c
@@ -12,7 +12,7 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int num = n, i, remainder = 0;
 
-	if (index >= size(n) * 8)
+	if (index >= sizeof(n) * 8)
 		return (-1);
 	for (i = 0; i <= index; i++)
 	{
