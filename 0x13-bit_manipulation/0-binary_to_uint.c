ae3693d1917fce5c74e4383a8737571cec2bdd6a
Jenn <jennogden95@gmail.com>
2018-08-09 08:16:07 +0000

    Adding task 0, README, and header file.

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string of 0 and 1 chars
+ *
+ * Return: converted number or 0 if the str contains an invalid
+ * char or is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec = 0, i = 0, j = 0, k = 0, power = 1;
+
+	if (!b)
+		return (0);
+	while (b[i] != '\0')
+		i++;
+	while (b[j] != '\0')
+	{
+		if (b[j] == (0 + '0') || b[j] == (1 + '0'))
+		{
+			while (k < i - 1)
+			{
+				power *= 2;
+				k++;
+			}
+			k = 0;
+			dec += (b[j] - '0') * power;
+			power = 1;
+			i--;
+		}
+		else
+			return (0);
+		j++;
+	}
+	return (dec);
+}
---
+++ 0x13-bit_manipulation/README.md
@@ -0,0 +1,5 @@
+# 0x13. C - Bit Manipulation
+## What you should learn from this project
+At the end of this project you are expected to be able to explain to anyone, without the help of Google:
+* Look for the right source of information without too much help
+* How to manipulate bits and use bitwise operators
