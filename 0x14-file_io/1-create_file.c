26947e6aa80ef976c6b00e36944031e6e5d00c94
Jenn <jennogden95@gmail.com>
2018-08-14 20:55:25 +0000

    Made sure file closes even upon error

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -17,19 +17,13 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename,  O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (text_content == NULL)
+	while (text_content[count])
+		count++;
+	written = write(fd, text_content, count);
+	if (written == -1)
 	{
-		written = write(fd, "\0", 1);
-		if (written == -1)
-			return (-1);
-	}
-	else
-	{
-		while (text_content[count])
-			count++;
-		written = write(fd, text_content, count);
-		if (written == -1)
-			return (-1);
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 	return (1);
