b47b05ee1dd4464be3cb7531f1538fdad5e6dea0
Jenn <jennogden95@gmail.com>
2018-08-14 20:59:46 +0000

    Checks if written is equal to count

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,13 +14,13 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename,  O_CREAT | O_RDWR | O_TRUNC, 0600);
+	fd = open(filename,  O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	while (text_content[count])
 		count++;
 	written = write(fd, text_content, count);
-	if (written == -1)
+	if (written == -1 || written != count)
 	{
 		close(fd);
 		return (-1);
