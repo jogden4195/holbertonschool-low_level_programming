4b53569b340558fa8715cfc689a26f595712637e
Jenn <jennogden95@gmail.com>
2018-08-14 21:10:35 +0000

    Added situation for when text is null

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -17,6 +17,11 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename,  O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	while (text_content[count])
 		count++;
 	written = write(fd, text_content, count);
