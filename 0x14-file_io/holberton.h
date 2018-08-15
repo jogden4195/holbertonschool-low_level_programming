5b9d218dad642ae5ce4748e4bd94512793611857
Jenn <jennogden95@gmail.com>
2018-08-15 16:03:25 +0000

    Fixed: Betty error

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -12,7 +12,7 @@
 #define EXIT98 "Error: Can't read from file %s\n"
 #define EXIT99  "Error: Can't write to %s\n"
 #define EXIT100 "Error: Can't close fd %d\n"
- 
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
