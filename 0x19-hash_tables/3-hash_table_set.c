5e63b4aac1aa3162d78810daf4de0447ca2ae840
Jenn <jennogden95@gmail.com>
2018-10-12 20:14:36 +0000

    Deleted print statements.

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,50 +14,45 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int i;
 	hash_node_t *new, *temp, *head;
 
+	
 	if (ht != NULL && key != NULL && value != NULL)
 	{
-		new = malloc(sizeof(hash_node_t));
-		if (new)
-		{
-			i = key_index((const unsigned char *)key, ht->size);
-			new->key = strdup(key);
-			if (!new->key)
-				return (0);
-			new->value = strdup(value);
-			if (!new->key)
-				return (0);
-			if (!(ht->array[i]))
-			{
-				printf("hello!");
-				ht->array[i] = new;
-				new->next = NULL;
-			}
-			else
-			{
-				head = ht->array[i];
-				temp = head;
-				printf("hi!");
-				while (temp->next)
-				{
-					printf("hi");
-					if (strcmp(temp->key, key) == 0)
-					{
-						printf("hi");
-						free(new->key);
-						free(new->value);
-						free(new);
-						free(temp->value);
-						temp->value = strdup(value);
-						printf("%s", temp->value);
-						return (1);
-					}
-					temp = temp->next;
-				}
-				new->next = head;
-				ht->array[i] = new;
-			}
-			return (1);
-		}
+		return (0);
 	}
-	return (0);
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+	i = key_index((const unsigned char *)key, ht->size);
+	new->key = strdup(key);
+	if (!new->key)
+		return (0);
+	new->value = strdup(value);
+	if (!new->key)
+		return (0);
+	if (!(ht->array[i]))
+	{
+		ht->array[i] = new;
+		new->next = NULL;
+	}
+	else
+	{
+		head = ht->array[i];
+		temp = head;
+		while (temp->next)
+		{
+			if (strcmp(temp->key, key) == 0)
+			{
+				free(new->key);
+				free(new->value);
+				free(new);
+				free(temp->value);
+				temp->value = strdup(value);
+				return (1);
+			}
+			temp = temp->next;
+		}
+		new->next = head;
+		ht->array[i] = new;
+	}
+	return (1);
 }
