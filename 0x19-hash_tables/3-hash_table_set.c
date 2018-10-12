6881a4c0220a1ebcb8a93fc3abc1cddec999d2de
Jenn <jennogden95@gmail.com>
2018-10-12 20:17:39 +0000

    Fixed betty error.

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int i;
 	hash_node_t *new, *temp, *head;
 
-	
+
 	if (ht == NULL || key == NULL || value == NULL)
 	{
 		return (0);
