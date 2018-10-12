d94ef50dcb7094c76abb17997112f4d1cc715649
Jenn <jennogden95@gmail.com>
2018-10-12 20:16:16 +0000

    Changed Null check in the beginning.

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *temp, *head;
 
 	
-	if (ht != NULL && key != NULL && value != NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 	{
 		return (0);
 	}
