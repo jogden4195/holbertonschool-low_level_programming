0a0216dff0cfb4fc439bf130818e1fc82caae413
Jenn <jennogden95@gmail.com>
2018-10-12 21:47:03 +0000

    Fixed memory leaks.

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i = 0;
 	hash_table_t *new;
 
 	if (size)
@@ -17,7 +18,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 		if (new)
 		{
 			new->size = size;
-			new->array = malloc(sizeof(unsigned long int) * size);
+			new->array = malloc(sizeof(hash_node_t *) * size);
+			if (!new)
+				return (NULL);
+			while (i < size)
+			{
+				new->array[i] = 0;
+				i++;
+			}
 			return (new);
 		}
 	}
--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -1,50 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: hash table we are adding/updating
- * @key: the key of the element we are adding
- * @value: the value of the element we are adding
+ * main - check the code for Holberton School students.
  *
- * Return: 1 if success, 0 if failure
+ * Return: Always EXIT_SUCCESS.
  */
-
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+int main(void)
 {
-	unsigned long int i;
-	hash_node_t *new, *temp;
+  hash_table_t *ht;
 
-	if (ht == NULL || key == NULL || *key == "\0" || value == NULL)
-		return (0);
-	new = malloc(sizeof(hash_node_t));
-	if (!new)
-		return (0);
-	i = key_index((const unsigned char *)key, ht->size);
-	new->key = strdup(key);
-	new->value = strdup(value);
-	if (!(ht->array[i]))
-	{
-		ht->array[i] = new;
-		new->next = NULL;
-	}
-	else
-	{
-		temp = ht->array[i];
-		while (temp->next)
-		{
-			if (strcmp(temp->key, key) == 0)
-			{
-				free(new->key);
-				free(new->value);
-				free(new);
-				free(temp->value);
-				temp->value = strdup(value);
-				return (1);
-			}
-			temp = temp->next;
-		}
-		new->next = ht->array[i];
-		ht->array[i] = new;
-	}
-	return (1);
+  ht = hash_table_create(1024);
+  hash_table_set(ht, "betty", "holberton");
+  return (EXIT_SUCCESS);
 }
