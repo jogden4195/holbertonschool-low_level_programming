3c3dfe4f7a7fd99d3787e73669b7015762a4368a
Jenn <jennogden95@gmail.com>
2018-12-12 21:57:57 +0000

    FIX: Added check for tree->parent

--- 0x1C-binary_trees/10-binary_tree_depth.c
+++ 0x1C-binary_trees/10-binary_tree_depth.c
@@ -13,7 +13,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	size_t depth = 0;
 
 	if (!tree)
-		return (-1);
-	depth = binary_tree_depth(tree->parent);
-	return (depth + 1);
+		return (0);
+	if (tree->parent)
+		depth = 1 + binary_tree_depth(tree->parent);
+	return (depth);
 }
